aa8f02e0771a714b08375e43661568c894fcdb34
Jens Keller <jens.keller@sap.com>
2015-10-13 11:52:20 +0200

    testing-ui5 exercise

--- src/test/java/com/sap/ase/exercises/decoupling/BetsTest.java
+++ src/test/java/com/sap/ase/exercises/decoupling/BetsTest.java
@@ -7,22 +7,30 @@ import org.junit.Test;
 
 public class BetsTest {
 
+	private class BetsForTesting extends Bets {
+
+		@Override
+		protected int getMaxAmount() {
+			return 10;
+		}
+	}
+	
 	@Test(expected = NotEnoughPlayers.class)
 	public void noBetPlaced_shouldThrow() {
-		Bets bets = new Bets();
+		Bets bets = new BetsForTesting();
 		bets.areEven();
 	}
 
 	@Test(expected = NotEnoughPlayers.class)
 	public void oneBetPlaced_shouldThrow() {
-		Bets bets = new Bets();
+		Bets bets = new BetsForTesting();
 		bets.bet("john", 1);
 		bets.areEven();
 	}
 
 	@Test
 	public void givenTwoBetsPlaced_sameAmount() {
-		Bets bets = new Bets();
+		Bets bets = new BetsForTesting();
 		bets.bet("john", 1);
 		bets.bet("frank", 1);
 		assertThat(bets.areEven(), is(true));
@@ -30,7 +38,7 @@ public class BetsTest {
 
 	@Test
 	public void givenTwoBetsPlaced_differentAmount() {
-		Bets bets = new Bets();
+		Bets bets = new BetsForTesting();
 		bets.bet("john", 1);
 		bets.bet("frank", 2);
 		assertThat(bets.areEven(), is(false));
